c2e43f0557fa1689ada323ec39ce24184be269f6
TotiManuel <manuel.mandaio@gmail.com>
2026-03-30 12:26:06 -0300

    fix?

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
 from flask import Flask
 from config import Config
-from extensions import db
 from routes.main import main
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
-    
-    db.init_app(app)
 
     app.register_blueprint(main)
 
--- config.py
+++ config.py
@@ -2,6 +2,3 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "mortal-secret")
-
-    SQLALCHEMY_DATABASE_URI = "sqlite:///tienda.db"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
